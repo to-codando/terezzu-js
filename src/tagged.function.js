94167c65d2cb2df2d795ace632894a94cad2bc22
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2023-01-05 01:10:54 -0300

    feat: update htm as peerDependency

--- src/tagged.function.js
+++ src/tagged.function.js
@@ -1,20 +1,19 @@
-import htm from 'htm';
+import htm from 'htm'
 
 const _taggedFn = (tags, ...values) => {
   return tags
     .map((tag, index) => {
-      return `${tag}${values[index] || ""}`;
+      return `${tag}${values[index] || ''}`
     })
-    .join("");
-};
+    .join('')
+}
 
 const css = _taggedFn
 
-
 function h(type, props, ...children) {
-  return { type, props, children };
+  return { type, props, children }
 }
 
-const html = htm.bind(h);
+const html = htm.bind(h)
 
 export { html, css }
