1150a85b7c02da3bb71b75dbf0495249db848c2d
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2023-01-04 22:41:14 -0300

    feat: update entrypoint

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { render } from './render.js'
-export { pubsubFactory } from './pubsub.factory.js'
-export { observerFactory } from './observer.factory.js'
-export { storeFactory } from './store.factory.js'
